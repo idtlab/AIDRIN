e778ce2c1f814801ca4c57da22c059f4162af9a4
Harish Balaji B <harishbalaji241@gmail.com>
2025-08-03 19:44:51 -0400

    lint error fixed for make_celery.py

--- aidrin/make_celery.py
+++ aidrin/make_celery.py
@@ -5,5 +5,3 @@ setup_logging()  # Initialize logging before creating the app
 flask_app = create_app()
 celery_app = flask_app.extensions["celery"]
 
-# Import tasks to register them with Celery
-from aidrin.structured_data_metrics.privacy_measure import calculate_single_attribute_risk_score, calculate_multiple_attribute_risk_score
