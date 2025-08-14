c925ce32b90331f4165b6f43aec04f05d835df8c
Kaveen Hiniduma <123603111+kaveenh@users.noreply.github.com>
2025-08-14 09:29:37 -0400

    Kaveen/readthedocs (#23)
    * docs init
    
    * docs minor update
    
    * docs minor update
    
    * contributions section added to doc
    
    * docs: add privacy metrics and class imbalance documentation
    
    * usage content added to docs
    
    * minor doc fix
    
    * validation fixes and improved layout
    
    * change logo to transparent background
    
    ---------
    
    Co-authored-by: Harish Balaji B <harishbalaji241@gmail.com>
    Co-authored-by: Jean Luca Bez <jlbez@lbl.gov>

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'AIDRIN'
+copyright = '2025, IDT Lab'
+author = 'IDT Lab'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'style_nav_header_background': '#efefef',
+    'logo_only': True,
+    'collapse_navigation': False,
+    'navigation_depth': 2,
+    'sticky_navigation': True,
+}
+
+html_logo = "_static/logo.png"
+html_static_path = ['_static']
+html_css_files = ['css/aidrin.css']
