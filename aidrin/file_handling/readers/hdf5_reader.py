b8987e6b034c5b9477ea979cedd69b1f3b062efe
varunviswapriyan <150558463+varunviswapriyan@users.noreply.github.com>
2025-12-07 19:51:05 -0500

    Add MAX_TOTAL_ROWS and MAX_ROWS_PER_DATASET variables

--- aidrin/file_handling/readers/hdf5_reader.py
+++ aidrin/file_handling/readers/hdf5_reader.py
@@ -14,6 +14,8 @@ class hdf5Reader(BaseFileReader):
         try:
             CHUNK = 10_000
 
+            MAX_TOTAL_ROWS = None
+            MAX_ROWS_PER_DATASET = None
 
             dfs = []
             total_rows = 0
